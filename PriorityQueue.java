850cf52a219dfdb34365deb02c63e9d2fb4a6fb7
ezequiel depetris <ezedepetris@gmail.com>
2014-11-15 18:48:25 -0300

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -8,7 +8,5 @@ public interface PriorityQueue{
 
 	public abstract Comparable dequeue();
 
-	//public static abstract void heapSort(Comparable [] array, int n);
-
 	public abstract boolean repOk();
 }
